970ddf99422a5372791b3443a506c19d31166529
Anna Afijan <ann.afijan@gmail.com>
2024-08-22 11:51:21 +0000

    Revert "add a switch to app js create an ind route"
    This reverts commit 061b41d23a4e7be728887c4ff34315493c194da7.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
+<<<<<<< HEAD
 import { Route, Switch } from 'react-router-dom';
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -10,18 +11,25 @@ import SignUpForm from "./pages/auth/SignUpForm";
 >>>>>>> parent of d30d2fc (install axios and create a signup form)
 =======
 >>>>>>> parent of d30d2fc (install axios and create a signup form)
+=======
+>>>>>>> parent of 061b41d (add a switch to app js create an ind route)
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
+<<<<<<< HEAD
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
+=======
+        <h1>Home page</h1>
+        <h1>Sign in</h1>
+>>>>>>> parent of 061b41d (add a switch to app js create an ind route)
       </Container>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -13,6 +14,18 @@ root.render(
     <Router>
       <App />
     </Router>
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+>>>>>>> parent of 061b41d (add a switch to app js create an ind route)
   </React.StrictMode>
 );
 
